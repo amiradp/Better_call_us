7e585bbbef70bbd311e46a46ec8686a125025788
amiradp <amiradp3@gmail.com>
2023-11-22 21:09:26 +0330

    add template and ui

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'django_bootstrap5',
 
     # my app
     'pages',
@@ -120,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- post/views.py
+++ post/views.py
@@ -8,3 +8,4 @@ from .models import Post
 class PostListView(generic.ListView):
     queryset = Post.objects.filter(active=True)
     template_name = 'post/postlist.html'
+    context_object_name = 'post_detail'
